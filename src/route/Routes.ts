67bca817a1f2ef54ca0878ba67fb2ff7c603788d
Christophe de Batz <christophe.db@gmail.com>
2018-02-02 21:29:34 +0100

    feat(route): add user creation route

--- src/route/Routes.ts
+++ src/route/Routes.ts
@@ -1,9 +1,13 @@
 import * as restify from 'restify';
+import UserController from '../controller/UserController';
 
 module.exports.routes = {
 
   initialize(api:restify.Server) {
+    const userController:UserController = new UserController();
 
+    // public resources
+    api.post('/users', userController.createUser);
   }
 
 };
